c18081707bc4175c2205f673d5b8e5f50b2bc167
Donutboy2003 <113123713+Donutboy2003@users.noreply.github.com>
2025-03-01 12:12:42 -0700

    Initial code for handling local server

---
+++ server.c
@@ -0,0 +1,42 @@
+#include <WiFi.h>
+#include <WebServer.h>
+
+const char *ssid = "EU Guest";         // Change this to your WiFi name
+const char *password = "GuestAccess!"; // Change this to your WiFi password
+
+WebServer server(80);
+
+int peopleCount = 0; // Example variable to store the count
+
+void handleRoot()
+{
+    String html = "<html><head><meta http-equiv='refresh' content='2'></head>"
+                  "<body><h1>People Count: " +
+                  String(peopleCount) + "</h1></body></html>";
+    server.send(200, "text/html", html);
+}
+
+void setup()
+{
+    Serial.begin(115200);
+    WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        delay(1000);
+        Serial.println("Connecting...");
+    }
+    Serial.println("Connected to WiFi");
+
+    server.on("/", handleRoot);
+    server.begin();
+    Serial.println("Server started!");
+}
+
+void loop()
+{
+    server.handleClient();
+
+    // Simulate people counting (replace with real sensor logic)
+    peopleCount = random(0, 20);
+    delay(2000);
+}
